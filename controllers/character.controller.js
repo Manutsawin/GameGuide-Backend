609a405b4ffb7b67dae110a716bb01fe50a6fc85
Manutsawin <manassawin369@gmail.com>
2022-06-27 16:07:46 +0700

    seccond

--- controllers/character.controller.js
+++ controllers/character.controller.js
@@ -33,3 +33,29 @@ exports.create = (req, res) => {
             });
         });
 };
+
+exports.getAll = (req, res) => {
+    Character.findAll()
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving tutorials."
+        });
+      });
+};
+
+exports.getOne = (req, res) => {
+    const id = req.params.id;
+    Character.findByPk(id)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Error retrieving Tutorial with id=" + id
+        });
+      });
+  };
